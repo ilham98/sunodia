340f7d1aa2d409936eadb9b577198ce87a6b09f5
ilham98 <ilhamshinohara@gmail.com>
2019-10-25 03:50:40 +0800

    fix forcehttps middleware

--- database/seeds/BeritaTableSeeder.php
+++ database/seeds/BeritaTableSeeder.php
@@ -32,7 +32,7 @@ class BeritaTableSeeder extends Seeder
 
         DB::table('berita')->insert([
             'judul' => 'Berita 3',
-            'isi' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p><p><br></p><p><img src="http://127.0.0.1:8000/uploads/_test-files/5db0a43c93295.jpg" style="" width="711"></p>',
+            'isi' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p><p><br></p><p><img src="'.$this->generateUrl('uploads/_test-files/5db0a43c93295.jpg').'" style="" width="711"></p>',
             'tingkat' => null
         ]);
     }
