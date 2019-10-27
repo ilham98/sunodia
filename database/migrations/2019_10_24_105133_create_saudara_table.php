8e3a81c3d4445affa04f2ec26f88d7d5e855cb38
ilham98 <ilhamshinohara@gmail.com>
2019-10-27 17:26:00 +0800

    fix saudara table

--- database/migrations/2019_10_24_105133_create_saudara_table.php
+++ database/migrations/2019_10_24_105133_create_saudara_table.php
@@ -17,7 +17,7 @@ class CreateSaudaraTable extends Migration
             $table->bigIncrements('id');
             $table->bigInteger('registrasi_siswa_id')->unsigned();
             $table->string('saudara_nama');
-            $table->int('saudara_umur');
+            $table->interger('saudara_umur');
             $table->string('saudara_pendidikan');
             $table->string('saudara_status');
             $table->foreign('registrasi_siswa_id')->references('id')->on('registrasi_siswa');
